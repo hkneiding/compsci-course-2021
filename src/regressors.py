fixed ridge, logistic derivatives to also include alpha value

src/regressors.py
@@ -84,7 +84,8 @@ def get_beta(type, model_matrix, targets, regressor_parameters):
                                                  learning_rate=regressor_parameters['learning_rate'], 
                                                  max_iterations=regressor_parameters['max_iterations'],
                                                  momentum=regressor_parameters['momentum'],
-                                                 batch_size=regressor_parameters['batch_size']
+                                                 batch_size=regressor_parameters['batch_size'],
+                                                 alpha=0
                                                 )
     elif type == RegressorType.LASSO:
         return calculate_beta_lasso(model_matrix, targets, alpha=regressor_parameters['alpha'])
@@ -93,11 +94,12 @@ def get_beta(type, model_matrix, targets, regressor_parameters):
     elif type == RegressorType.RIDGE_SGD:
         # randomly generate first beta
         beta = np.random.normal(size = model_matrix.shape[1])
-        return stochastic_batch_gradient_descent(model_matrix, targets, beta, calculate_cost_derivative_mse, 
+        return stochastic_batch_gradient_descent(model_matrix, targets, beta, calculate_cost_derivative_ridge, 
                                                  learning_rate=regressor_parameters['learning_rate'], 
                                                  max_iterations=regressor_parameters['max_iterations'],
                                                  momentum=regressor_parameters['momentum'],
-                                                 batch_size=regressor_parameters['batch_size']
+                                                 batch_size=regressor_parameters['batch_size'],
+                                                 alpha=regressor_parameters['alpha']
                                                 )
     elif type == RegressorType.LOGISTIC:
         beta = np.random.normal(size = model_matrix.shape[1])
@@ -105,7 +107,8 @@ def get_beta(type, model_matrix, targets, regressor_parameters):
                                                  learning_rate=regressor_parameters['learning_rate'], 
                                                  max_iterations=regressor_parameters['max_iterations'],
                                                  momentum=regressor_parameters['momentum'],
-                                                 batch_size=regressor_parameters['batch_size']
+                                                 batch_size=regressor_parameters['batch_size'],
+                                                 alpha=regressor_parameters['alpha'],
                                                 )
     else:
         raise NotImplementedError('Regressor type not implemented')

src/tools.py
@@ -17,21 +17,21 @@ def calculate_cost_mse(predictions, targets):
 def calculate_accuracy(predictions, targets):
     return 1 - np.sum(np.absolute(predictions - targets))/len(targets)
 
-def calculate_cost_derivative_mse(X, Y, beta):
+def calculate_cost_derivative_mse(X, Y, beta, alpha=0):
 
-    return 1 / len(X) * (X @ beta - Y) @ X
+    return 1 / len(X) * (X @ beta - Y) @ X + 2 * alpha * beta
 
-def calculate_cost_derivative_ridge(X, Y, beta):
+def calculate_cost_derivative_ridge(X, Y, beta, alpha):
 
-    return 1 / len(X) * (X @ beta - Y) @ X + 2 * beta
+    return 1 / len(X) * (X @ beta - Y) @ X + 2 * alpha * beta
 
 def calculate_sigmoid(array_data):
     return 1/(1 + np.exp(-array_data))
 
-def calculate_cost_derivative_logistic(X, Y, beta):
+def calculate_cost_derivative_logistic(X, Y, beta, alpha=0):
     weights_applied =  np.dot(X, beta)
     prediction = calculate_sigmoid(weights_applied)
-    return 1 / X.shape[0] * (X.T @ (prediction - Y))
+    return 1 / X.shape[0] * (X.T @ (prediction - Y)) + 2 * alpha * beta
 
 def scale_min_max(x):
 